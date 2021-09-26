c46c4fe1d3b6db8ffb4b6fe34898bf4c8ba25199
yen-tingLin <aillvone@gmail.com>
2021-09-27 07:30:51 +0800

    revise the file path

--- routes/fileRoute.js
+++ routes/fileRoute.js
@@ -10,21 +10,21 @@ router.get("", (req, res) => res.send("RESTful API for File System"));
 /**
  * 1 ~ 3. Request GET - if the request target is file / directory
  */
-router.get("/files/*", FileController.getFiles);
+router.get("/file/*", FileController.getFiles);
 
 /**
  * 4. Request POST - To create a file in specified {localSystemFilePath}
  */
-router.post("/files/*", FileController.createFile);
+router.post("/file/*", FileController.createFile);
 
 /**
  * 5. Request PATCH - To update file in specified {localSystemFilePath}
  */
-router.patch("/files/*", FileController.updateFile);
+router.patch("/file/*", FileController.updateFile);
 
 /**
  * 6. Request Delete - To delete file in specified {localSystemFilePath}
  */
-router.delete("/files/*", FileController.deleteFile);
+router.delete("/file/*", FileController.deleteFile);
 
 module.exports = router;
